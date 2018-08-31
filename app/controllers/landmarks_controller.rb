7c48cc7cbc8f0e8c5ceecf4110f644251a2dd000
Laura <laurao148@gmail.com>
2018-08-31 00:37:51 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,20 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  get 'landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(:name => params["Name"])
+    @landmark.title = Title.find(:name => params["Title Name"])
+    @landmark.title_id = params[:titles]
+    @landmark.save
+    redirect to("/landmarks/#{@landmark.id}")
+  end
+
 end
